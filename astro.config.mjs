03d7789897ae965dfd1d8ec517105a16e3a78915
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2025-05-21 12:15:11 -0300

    Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import yaml from '@rollup/plugin-yaml';
 import sitemap from "@astrojs/sitemap";
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.kodular.io',
-  integrations: [sitemap()],
+  integrations: [sitemap(), preact()],
   vite: {
     plugins: [yaml()]
   },
